48e00abbb3a5889c6aa3d5c422b2e638429e7af0
danozgriff <dane.griff@gmail.com>
2015-12-01 22:44:12 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,6 +15,5 @@ br = mechanize.Browser()
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 response = br.open(url)
 
-for pagenum in range(10):
     html = response.read()
-    print re.findall("MainContent_CompanyTicker*span", html)
+    print re.findall('[...].AX', html)
