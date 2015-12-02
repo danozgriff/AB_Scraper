5d8c1bef6564498930595ba815f7173825aa4fdd
danozgriff <dane.griff@gmail.com>
2015-12-02 13:32:28 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,6 +22,6 @@ for pagenum in range(1):
   # test1 = re.search(r'MainContent_CompanyTicker((.)+)span', html).group(0)
   
 #print re.findall(r'MainContent_signalpagehistory_PatternHistory24((.)+)\<\\table\>', html) 
-print re.findall(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)table', html) 
+print re.findall(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)\<\/table\>', html) 
    
   # print re.search(r'\w{3}\.AX', re.search(r'MainContent_CompanyTicker((.)+)span', html).group(0)).group(0)
