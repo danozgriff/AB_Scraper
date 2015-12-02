493f430bf427e3e1188635be1171562db6810178
danozgriff <dane.griff@gmail.com>
2015-12-02 09:43:22 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,6 +18,6 @@ response = br.open(url)
 for pagenum in range(1):
    html = response.read()
     
-   print re.search(r'MainContent_CompanyTicker((.)+)span', html)
+   print re.search(r'MainContent_CompanyTicker((.)+)span', html).group(0)
    
    # print re.search(r'\w{3}\.AX', html).group(0)
