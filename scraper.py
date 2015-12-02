b3f2f3121bb79e0dcfe31e4049c22973437be46b
danozgriff <dane.griff@gmail.com>
2015-12-02 21:50:06 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -25,7 +25,7 @@ for pagenum in range(1):
 test1 = re.search(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)MainContent_signalpagehistory_PatternHistory24_IADD', html).group(0) 
 # test2 = re.findall(r'(\"\>|img\/)((.)+)\<\/td\>\<td', test1)
 #test2 = re.findall(r'\"\>(.*)(\<\/td\>\<td )', test1)
-test2 = re.findall(r'\"\>(.*)(\<\/)', test1)
+test2 = re.findall(r'\"\>(.*)\<\/', test1)
 print test2[0]
 
    
