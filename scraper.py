127cdeffbd92094c766670cedd94a37d2cf238a9
danozgriff <dane.griff@gmail.com>
2015-12-02 09:36:25 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,6 +18,6 @@ response = br.open(url)
 for pagenum in range(1):
    html = response.read()
     
-   print re.findall(r'MainContent_CompanyTicker[a-z0-9A-Z\B]+span', html)
+   print re.findall(r'MainContent_CompanyTicker[\w+\s+\d+]+span', html)
    
    # print re.search(r'\w{3}\.AX', html).group(0)
