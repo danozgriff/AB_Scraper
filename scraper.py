5ff4c5a0c4e06199d19b3ea759dbc32379a94e28
danozgriff <dane.griff@gmail.com>
2015-12-08 16:33:09 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ import csv
 # This example shows how to follow the Next page link
 
 data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
-url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=TOT.AX'
+url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=WOW.AX'
 br = mechanize.Browser()
 
     # sometimes the server is sensitive to this information
