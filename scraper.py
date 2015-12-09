67bd18395fdd804f7789c382a048ae1072c21c75
danozgriff <dane.griff@gmail.com>
2015-12-09 11:59:55 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,8 +12,9 @@ import sqlite3
 
 asxlist = scraperwiki.sqlite.execute("select `ASX code` from company limit 5")
 
-print asxlist["ASX code"][0]
-
+#print asxlist["ASX code"][0]
+for x in asxlist:
+    print x
 
 data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
 url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=WOW.AX'
