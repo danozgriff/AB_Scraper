f6c8eaafa2e8c358b35be085ddd03c76de01722c
danozgriff <dane.griff@gmail.com>
2015-12-09 10:14:22 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -26,7 +26,7 @@ br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.
 #scraperwiki.sqlite.execute("drop table if exists company")  
 #scraperwiki.sqlite.execute("create table company (`GICS industry group` string, `ASX code` string, `Company name` string, `Last Refreshed` date, `Top 500` char(1))")
 scraperwiki.sqlite.execute("drop table if exists Signal_History")  
-scraperwiki.sqlite.execute("create table Signal_History (`ASX code` varchar2(8) NOT NULL, `Date` date PRIMARY KEY NOT NULL, `Price` real NOT NULL, `Signal` varchar2(15) NOT NULL, `Confirmation` char(1) NOT NULL, `AUD 100` real NOT NULL, PRIMARY KEY (`ASX code`, `Date`)")
+scraperwiki.sqlite.execute("create table Signal_History (`ASX code` varchar2(8) NOT NULL, `Date` date PRIMARY KEY NOT NULL, `Price` real NOT NULL, `Signal` varchar2(15) NOT NULL, `Confirmation` char(1) NOT NULL, `AUD 100` real NOT NULL, PRIMARY KEY (`ASX code`, `Date`))")
 
 #scraperwiki.sqlite.execute("insert into company values ('test', 'test', 'test', date('2015-12-07'), 'Y')")
 #scraperwiki.sqlite.execute("delete from company where `ASX code` = 'test'")
