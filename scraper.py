3974e9d3dba45611744de6374a47afa680311b78
danozgriff <dane.griff@gmail.com>
2015-12-09 12:30:15 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,9 @@ import scraperwiki
 import mechanize
 import re
 import csv
-import sqlite3
+#import sqlite3
+
+url = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker='
 
 # ASPX pages are some of the hardest challenges because they use javascript and forms to navigate
 # Almost always the links go through the function function __doPostBack(eventTarget, eventArgument)
@@ -13,18 +15,18 @@ import sqlite3
 asxlist = scraperwiki.sqlite.execute("select `ASX code` from company limit 5")
 
 for x in asxlist["data"]:
-    print str(x)[3:-2]
+    asxcode = str(x)[3:-2] + '.AX'
     #print str(asxlist["data"][x])[3:-2]
 
 #for x in asxlist:
 #    print asxlist
 
-data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
-url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=WOW.AX'
-br = mechanize.Browser()
+#data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
+#url2 = 'https://www.aussiebulls.com/SignalPage.aspx?lang=en&Ticker=WOW.AX'
+    br = mechanize.Browser()
 
     # sometimes the server is sensitive to this information
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 #####reader = csv.DictReader(data.splitlines()[2:10])
 
@@ -35,8 +37,8 @@ br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.
 
 #scraperwiki.sqlite.execute("drop table if exists company")  
 #scraperwiki.sqlite.execute("create table company (`GICS industry group` string, `ASX code` string, `Company name` string, `Last Refreshed` date, `Top 500` char(1))")
-scraperwiki.sqlite.execute("drop table if exists Signal_History")  
-scraperwiki.sqlite.execute("create table Signal_History (`ASX code` varchar2(8) NOT NULL, `Date` date NOT NULL, `Price` real NOT NULL, `Signal` varchar2(15) NOT NULL, `Confirmation` char(1) NOT NULL, `AUD 100` real NOT NULL, UNIQUE (`ASX code`, `Date`))")
+#scraperwiki.sqlite.execute("drop table if exists Signal_History")  
+#scraperwiki.sqlite.execute("create table Signal_History (`ASX code` varchar2(8) NOT NULL, `Date` date NOT NULL, `Price` real NOT NULL, `Signal` varchar2(15) NOT NULL, `Confirmation` char(1) NOT NULL, `AUD 100` real NOT NULL, UNIQUE (`ASX code`, `Date`))")
 
 #scraperwiki.sqlite.execute("insert into company values ('test', 'test', 'test', date('2015-12-07'), 'Y')")
 #scraperwiki.sqlite.execute("delete from company where `ASX code` = 'test'")
@@ -60,19 +62,19 @@ scraperwiki.sqlite.execute("create table Signal_History (`ASX code` varchar2(8) 
 #####for record in reader:
 #####        print record
 
-response = br.open(url2)
+    response = br.open(url + asxcode)
 
-for pagenum in range(1):
-   html = response.read()
+    for pagenum in range(1):
+        html = response.read()
     
 #comp = re.search(r'MainContent_CompanyTicker(\w{3,}\.AX)span', html).group(0)
-ASX_Code = '3PL.AX'
+#ASX_Code = '3PL.AX'
   
 #print re.findall(r'MainContent_signalpagehistory_PatternHistory24((.)+)\<\\table\>', html) 
-test1 = re.search(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)MainContent_signalpagehistory_PatternHistory24_IADD', html)
+        test1 = re.search(r'MainContent_signalpagehistory_PatternHistory24_DXDataRow0((.|\n)+)MainContent_signalpagehistory_PatternHistory24_IADD', html)
 
-if test1:
-    test1 = test1.group(0)
+        if test1:
+            test1 = test1.group(0)
     
 
 # test2 = re.findall(r'(\"\>|img\/)((.)+)\<\/td\>\<td', test1)
@@ -85,7 +87,7 @@ if test1:
 #test2 = re.findall('(\">|img\/)(.*?)(<\/|\.gif)', test1.replace("\B", ""))
 
 #test3 = re.findall('\', \'(.*?)\', \'', test2)
-    test3 = re.findall('(\">|img\/)(.*?)(<\/|\.gif)', test1.replace("\B", ""))
+            test3 = re.findall('(\">|img\/)(.*?)(<\/|\.gif)', test1.replace("\B", ""))
 #print "".join(test3[0].split)
 #test4 = [x.replace(" ", "") for x in test3]
 
@@ -95,20 +97,20 @@ if test1:
 #print len(test3)
 #print ""
 
-    while len(test3) >= 5:
+            while len(test3) >= 5:
     
-        #print ASX_Code
-        sh_Date = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
-        sh_Price = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
-        sh_Signal = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
-        sh_Confirmation = (re.search("[Unc|C]heck", str(test3.pop(0)).replace(" ", "")).group(0).lower().replace("uncheck","N")).replace("check", "Y")
-        sh_AUD100 = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
+                #print ASX_Code
+                sh_Date = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
+                sh_Price = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
+                sh_Signal = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
+                sh_Confirmation = (re.search("[Unc|C]heck", str(test3.pop(0)).replace(" ", "")).group(0).lower().replace("uncheck","N")).replace("check", "Y")
+                sh_AUD100 = re.search("(\w|\d)(.*)(\w|\d)", str(test3.pop(0)).replace(" ", "")).group(0)
         
         #scraperwiki.sqlite.execute("insert or replace into Signal_History values (:`ASX code`, :Date, :Price, :Signal, :Confirmation, :`AUD 100`)",  {"ASX code":ASX_Code, "Date":sh_Date, "Price":sh_Price, "Signal":sh_Signal, "Confirmation":sh_Confirmation, "AUD 100":sh_AUD100})
 
-        scraperwiki.sqlite.execute("insert or ignore into Signal_History values (?, ?, ?, ?, ?, ?)",  [ASX_Code, sh_Date, sh_Price, sh_Signal, sh_Confirmation, sh_AUD100]) 
+                scraperwiki.sqlite.execute("insert or ignore into Signal_History values (?, ?, ?, ?, ?, ?)",  [asxcode, sh_Date, sh_Price, sh_Signal, sh_Confirmation, sh_AUD100]) 
 
-scraperwiki.sqlite.commit()    
+                scraperwiki.sqlite.commit()    
 
    
   # print re.search(r'\w{3}\.AX', re.search(r'MainContent_CompanyTicker((.)+)span', html).group(0)).group(0)
