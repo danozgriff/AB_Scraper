d4155022f8cef9cbeef584a75f612760750c8a5a
danozgriff <dane.griff@gmail.com>
2020-06-18 22:59:22 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,21 +5,8 @@ import csv
 #import time
 #from datetime import datetime, date
 #import datetime
+import requests
 
-
-#br = mechanize.Browser()
-
-    # sometimes the server is sensitive to this information
-#br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
-data = scraperwiki.scrape("https://www.listcorp.com/_api/services/discovery/download-companies-list")
-print data
-
-#scraperwiki.sqlite.execute("drop table if exists company")  
-#scraperwiki.sqlite.execute("create table company (`Code` string, `Company` string, `Market Cap` real, `Last Trade` real, `Change` real, `% Change` real, `Sector` string)")
-  
-#scraperwiki.sqlite.save(['Code', 'Company', 'Market Cap', 'Last Trade', 'Change', '% Change', 'Sector'], list(csv.DictReader(scraperwiki.scrape('https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false').splitlines()[1:])), table_name='company')
-#scraperwiki.sqlite.execute("update company set `Last Refreshed` = date('now') where `Last Refreshed` is null")
-#scraperwiki.sqlite.execute("update company set `Top 500` = 'Y' where `Last Refreshed` = date('now')")
-#scraperwiki.sqlite.execute("update company set `Top 500` = 'N' where `Last Refreshed` <> date('now')")
-#scraperwiki.sqlite.commit()
+URL = 'https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false'
+page = requests.get(URL)
+print page
