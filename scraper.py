306e60076bcd34d97fa72daf773568a5c739b900
danozgriff <dane.griff@gmail.com>
2020-06-18 23:01:15 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,8 +5,16 @@ import csv
 #import time
 #from datetime import datetime, date
 #import datetime
-import requests
+#import requests
 
-URL = 'https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false'
-page = requests.get(URL)
-print page
+#URL = 'https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false'
+#page = requests.get(URL)
+#print page
+
+#br = mechanize.Browser()
+
+    # sometimes the server is sensitive to this information
+#br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+data = scraperwiki.scrape("https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false")
+print data
