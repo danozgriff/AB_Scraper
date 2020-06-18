55379b50f19c03f95bbe65f0d28b4d674b9087bb
danozgriff <dane.griff@gmail.com>
2020-06-19 00:09:44 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,17 +1,10 @@
 import scraperwiki
 import mechanize
 import cookielib
-#import re
-import csv
-#import time
-#from datetime import datetime, date
-#import datetime
-#import requests
+from bs4 import BeautifulSoup
+
 
 url = 'https://www.marketindex.com.au/asx-listed-companies'
-#page = requests.get(URL)
-#print page
-
 br = mechanize.Browser()
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
@@ -19,25 +12,24 @@ br.set_handle_equiv(True)
 br.set_handle_redirect(True)
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
     # sometimes the server is sensitive to this information
 br.addheaders = [('User-agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36')]
 
-response = br.open(url)
-for pagenum in range(1):
-    html = response.read()
-print html
+page = br.open(url)
+htmlcontent = page.read()
+soup = BeautifulSoup(htmlcontent)
 
-#data = scraperwiki.scrape("https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false")
-#print data
+##print soup
 
+table = soup.find("asx_sp_table")
+
+output_rows = []
+for table_row in table.findAll('tr'):
+    columns = table_row.findAll('td')
+    output_row = []
+    for column in columns:
+        output_row.append(column.text)
+    output_rows.append(output_row)
     
-
-#scraperwiki.sqlite.execute("drop table if exists company")  
-#scraperwiki.sqlite.execute("create table company (`Code` string, `Company` string, `Market Cap` real, `Last Trade` real, `Change` real, `% Change` real, `Sector` string)")
-  
-#scraperwiki.sqlite.save(['Code', 'Company', 'Market Cap', 'Last Trade', 'Change', '% Change', 'Sector'], list(csv.DictReader(scraperwiki.scrape('https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false').splitlines()[1:])), table_name='company')
-#scraperwiki.sqlite.execute("update company set `Last Refreshed` = date('now') where `Last Refreshed` is null")
-#scraperwiki.sqlite.execute("update company set `Top 500` = 'Y' where `Last Refreshed` = date('now')")
-#scraperwiki.sqlite.execute("update company set `Top 500` = 'N' where `Last Refreshed` <> date('now')")
-#scraperwiki.sqlite.commit()
+    
+for p in output_rows: print p
