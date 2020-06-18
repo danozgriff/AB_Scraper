3c4779bc66f798b8b690dc8c97e5335cea8b02fa
danozgriff <dane.griff@gmail.com>
2020-06-18 22:52:54 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,8 @@ import csv
     # sometimes the server is sensitive to this information
 #br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-#data = scraperwiki.scrape("http://www.asx.com.au/asx/research/ASXListedCompanies.csv")
+data = scraperwiki.scrape("https://www.listcorp.com/_api/services/discovery/download-companies-list")
+print data
 
 #scraperwiki.sqlite.execute("drop table if exists company")  
 #scraperwiki.sqlite.execute("create table company (`Code` string, `Company` string, `Market Cap` real, `Last Trade` real, `Change` real, `% Change` real, `Sector` string)")
@@ -22,11 +23,3 @@ import csv
 #scraperwiki.sqlite.execute("update company set `Top 500` = 'Y' where `Last Refreshed` = date('now')")
 #scraperwiki.sqlite.execute("update company set `Top 500` = 'N' where `Last Refreshed` <> date('now')")
 #scraperwiki.sqlite.commit()
-
-
-input_file = csv.DictReader(open("https://www.listcorp.com/_api/services/discovery/download-companies-list"))
-#You may iterate over the rows of the csv file by iterating ove input_file. (Similarly to other files, you need to re-open the file if you want to iterate a second time.)
-
-for row in input_file:
-    print row
-    
