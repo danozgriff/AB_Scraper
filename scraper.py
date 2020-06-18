c6ceb5d8b82ef9f142efae9a2ccb608002ab6e23
danozgriff <dane.griff@gmail.com>
2020-06-18 23:11:41 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import scraperwiki
-#import mechanize
+import mechanize
 #import re
 import csv
 #import time
@@ -7,14 +7,16 @@ import csv
 #import datetime
 #import requests
 
-#URL = 'https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false'
+url = 'https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false'
 #page = requests.get(URL)
 #print page
 
-#br = mechanize.Browser()
+br = mechanize.Browser()
 
     # sometimes the server is sensitive to this information
-#br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-data = scraperwiki.scrape("https://www.listcorp.com/_api/services/discovery/download-companies-list?sortBy=market_capitalisation&descending=true&recentlyListedCompanies=false")
-print data
+response = br.open(url)
+for pagenum in range(1):
+    html = response.read()
+print html
