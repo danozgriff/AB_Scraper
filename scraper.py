6240efc667ca289adc2638477e8f18f4396a1504
danozgriff <dane.griff@gmail.com>
2020-06-19 15:51:10 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,67 +18,37 @@ br.addheaders = [('User-agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/
 
 
 scraperwiki.sqlite.execute("drop table if exists company")  
-scraperwiki.sqlite.execute("create table company (`Rank` string, `Code` string, `Company` string, `Price` real, `Change` real, `% Change` real, `% Change 1 Year` real, `Market Cap` real)")
-
+scraperwiki.sqlite.execute("create table company (`Rank` string, `Code` string, `Company` string, `Price` real, `Change` real, `% Change` real, `% Change 1 Year` real, `Market Cap` integer)")
 
 
 page = br.open(url)
 htmlcontent = page.read()
 soup = BeautifulSoup(htmlcontent, features="lxml")
 
-##print soup
 
-#table = soup.find("asx_sp_table")
 table = soup.find( "table", {"id":"asx_sp_table"} )
 
-#i=0
+
 output_rows = []
 for table_row in table.findAll('tr'):
     columns = table_row.findAll('td')
     output_row = []
     for column in columns:
         output_row.append(column.text + ",")
-        #if i < 100:
-            #print column.text
-            #i += 1
     output_rows.append(output_row)
     
-#output_rows = [item.replace("u'", "").replace("'", "").replace("\n", "").replace("[", "").replace("]", "") for item in output_rows]
 
-#output_rows = str(row).replace("u'", "").replace("'", "").replace("\n", "").replace("[", "").replace("]", "").split(',')
-
-#output_rows = 
-
-#for p in output_rows: print str(p).split(',')
-
-#.replace("u'", "").replace("'", "").replace("\n", "").replace("[", "").replace("]", "")
-
-#for p in output_rows:
-#    print [val for val in str(p).split()]
-#outputlst = []
-#i=0
 for sublst in output_rows:
     if len(sublst) > 0:
-      #  for item in sublst:
-     #       outputlst.append(item,)
-     
         rank = sublst[0].replace(",", "")
         code = sublst[2].replace(",", "") 
         company = sublst[3].replace(",", "") 
         price = sublst[4].replace(",", "").replace("$", "") 
         change = sublst[5].replace(",", "").replace("+", "")
-        #float(x.strip('"'))
         perchg = round(float(sublst[6].replace(",", "").replace("+", "").replace("%", "").strip('"'))/100.0, 4)                                                                                                   
         yrperchg = round(float(sublst[8].replace(",", "").replace("+", "").replace("%", "").strip('"'))/100.0, 4)                                                                                                 
         marketcap = sublst[7].replace(",", "").replace("$", "").replace(".", "").replace(" B", "0000000").replace(" M", "0000").replace(" TH", "0")    
         
         scraperwiki.sqlite.execute("insert or ignore into company values (?, ?, ?, ?, ?, ?, ?, ?)",  [rank, code, company, price, change, perchg, yrperchg, marketcap]) 
 
-   # 
-    #outputlst.clear()
-        #print item,        # note the ending ','
-        #i += 1
-        #print "count: " + str(i)
-    #i=0
-#    print 
 scraperwiki.sqlite.commit()  
