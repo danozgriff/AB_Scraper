66caee161b2ab61b9f939296815a5d5af20e638d
danozgriff <dane.griff@gmail.com>
2020-09-15 22:20:56 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,10 +18,10 @@ eoddate = None
 #scraperwiki.sqlite.execute("drop table if exists RunHistory")  
 #scraperwiki.sqlite.execute("create table RunHistory (`Start_DateTime` string NOT NULL, `End_DateTime` string, `Company_EOD_Date` string, UNIQUE (`Start_DateTime`, `Company_EOD_Date`))")
 
-if 1==1:
+#if 1==1:
 
-    scraperwiki.sqlite.execute("insert or replace into RunHistory values (?, ?, ?)",  [dtstart, dtend, eoddate]) 
-    scraperwiki.sqlite.commit() 
+#    scraperwiki.sqlite.execute("insert or replace into RunHistory values (?, ?, ?)",  [dtstart, dtend, eoddate]) 
+#    scraperwiki.sqlite.commit() 
 
 if 1==1:
 
@@ -171,6 +171,6 @@ if 1==1:
 
     dtend = datetime.now(tz=au_tz).strftime("%Y-%m-%d %H:%M:%S")
      
-    scraperwiki.sqlite.execute("insert or replace into RunHistory values (?, ?, ?)",  [dtstart, dtend, eoddate])     
+    scraperwiki.sqlite.execute("insert into RunHistory values (?, ?, ?)",  [dtstart, dtend, eoddate])     
     scraperwiki.sqlite.commit() 
     
